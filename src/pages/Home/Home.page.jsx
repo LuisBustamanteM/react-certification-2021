515d4a2651327e314710cb3c5bd1b6bd4e95b30f
Luis Bustamante <luis.bustamante@wizeline.com>
2021-10-11 10:46:36 -0500

    Challenge 1 & Removed unused files

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,8 +1,6 @@
 import React, { useRef } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 
-import { useAuth } from '../../providers/Auth';
-
 import Navbar from  '../../components/Navbar';
 import CardGrid from '../../components/CardGrid';
 import {Title} from './style';
@@ -10,13 +8,6 @@ import {Title} from './style';
 function HomePage() {
   const history = useHistory();
   const sectionRef = useRef(null);
-  const { authenticated, logout } = useAuth();
-
-  function deAuthenticate(event) {
-    event.preventDefault();
-    logout();
-    history.push('/');
-  }
 
   return (
     <section className="homepage" ref={sectionRef}>
